03fb7c0c62abf7e939c4106ca35361f3876ce057
Sergey Volkov <js2me@outlook.com>
2024-11-26 18:08:46 +0300

    fix: typings for parent page view models

--- src/page-view-model/page-view-model.ts
+++ src/page-view-model/page-view-model.ts
@@ -2,7 +2,7 @@ import { ViewModel } from 'mobx-vm-entities';
 
 export interface PageViewModel<
   Params extends Record<string, string> = Record<string, string>,
-  ParentViewModel extends ViewModel<any> = ViewModel<any, any>,
+  ParentViewModel extends ViewModel<any, any> = ViewModel<any, any>,
 > extends ViewModel<Params, ParentViewModel> {
   pathParams: Params;
 }
