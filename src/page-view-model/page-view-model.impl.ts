686a6f43aeb9349e2822f7f3a0fca0ce404e27f5
Sergey Volkov <js2me@outlook.com>
2025-01-02 20:30:32 +0300

    fix: default path params for page view model interface and implementation (empty object)

--- src/page-view-model/page-view-model.impl.ts
+++ src/page-view-model/page-view-model.impl.ts
@@ -1,9 +1,10 @@
 import { AnyViewModel, ViewModelImpl } from 'mobx-vm-entities';
+import { EmptyObject } from 'mobx-vm-entities/utils/types';
 
 import { PageViewModel } from './page-view-model';
 
 export class PageViewModelImpl<
-    Params extends Record<string, string> = Record<string, string>,
+    Params extends Record<string, string> = EmptyObject,
     ParentViewModel extends AnyViewModel | null = null,
   >
   extends ViewModelImpl<Params, ParentViewModel>
--- src/page-view-model/page-view-model.ts
+++ src/page-view-model/page-view-model.ts
@@ -1,7 +1,8 @@
 import { AnyViewModel, ViewModel } from 'mobx-vm-entities';
+import { EmptyObject } from 'mobx-vm-entities/utils/types';
 
 export interface PageViewModel<
-  Params extends Record<string, string> = Record<string, string>,
+  Params extends Record<string, string> = EmptyObject,
   ParentViewModel extends AnyViewModel | null = null,
 > extends ViewModel<Params, ParentViewModel> {
   pathParams: Params;
